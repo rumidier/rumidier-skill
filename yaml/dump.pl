762901d31d2efdce3aac1d0649ca6709c0df9e8a
Hanyoung Cho <rumidier@gmail.com>
2011-12-09 12:06:07 +0900

    git 단축키 설정, yaml-test

--- yaml/dump.pl
+++ yaml/dump.pl
@@ -70,6 +70,7 @@ my %dummy = (
     ]
 );
 
+# Changing data
 my $yaml = YAML::Tiny->new;
 $yaml->[0] = {
     site => \%site,
@@ -77,4 +78,49 @@ $yaml->[0] = {
     dummy => \%dummy,
 };
 
+#Add an entire document
+$yaml->[1] = [ 'foo', 'bar', 'baz' ];
+
+#./dump.pl
+#next step read file
 $yaml->write( 'just-test.yml' );
+
+ 
+=pod
+---
+rootproperty: blah
+section:
+  one: two
+  three: four
+  Foo: Bar
+  empty: ~
+ 
+ 
+ 
+#############################################
+# In your program
+ 
+use YAML::Tiny;
+ 
+# Create a YAML file
+my $yaml = YAML::Tiny->new;
+ 
+# Open the config
+$yaml = YAML::Tiny->read( 'file.yml' );
+ 
+# Reading properties
+my $root = $yaml->[0]->{rootproperty};
+my $one  = $yaml->[0]->{section}->{one};
+my $Foo  = $yaml->[0]->{section}->{Foo};
+ 
+# Changing data
+$yaml->[0]->{newsection} = { this => 'that' }; # Add a section
+$yaml->[0]->{section}->{Foo} = 'Not Bar!';     # Change a value
+delete $yaml->[0]->{section};                  # Delete a value
+ 
+# Add an entire document
+$yaml->[1] = [ 'foo', 'bar', 'baz' ];
+ 
+# Save the file
+$yaml->write( 'file.conf' );
+=cut
